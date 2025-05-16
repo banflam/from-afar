3d3d66df66fbee39f3bbf0d9f99fa418e8a18a62
banflam <rishiagrawal2002@gmail.com>
2025-05-16 17:10:27 -0400

    updated the layout file based off of the new auth provided wrapper

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,6 @@
 // app/layout.tsx
-import { AuthProvider } from "react-oidc-context";
 import React from "react";
-
-const cognitoAuthConfig = {
-  authority: "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_XymIyz8DE",
-  client_id: "367g3sp8609p8pf8e22p13n3ta",
-  redirect_uri: "https://d84l1y8p4kdic.cloudfront.net", // or http://localhost:3000 during dev
-  response_type: "code",
-  scope: "email openid phone",
-};
+import AuthProviderWrapper from "@/components/AuthProviderWrapper";
 
 export default function RootLayout({
   children,
@@ -18,7 +10,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <AuthProvider {...cognitoAuthConfig}>{children}</AuthProvider>
+        <AuthProviderWrapper>{children}</AuthProviderWrapper>
       </body>
     </html>
   );
