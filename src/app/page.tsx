dfba495030cb22ebaa7b4efac6d5a6061047f8ae
banflam <rishiagrawal2002@gmail.com>
2025-05-15 23:46:15 -0400

    added very basic homepage information

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <h1>Welcome to From Afar: friends at heart!</h1>
+      <p>The goal of this project is to replicate the magic and excitment of being pen pals with people all over the world -- with the wait and without the hassle</p>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
       </footer>
