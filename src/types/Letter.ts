65c7ef27eb4aa93292fdea9ed7dc62d4c80ff656
banflam <rishiagrawal2002@gmail.com>
2025-05-19 21:59:11 -0400

    added Letter interface to define the structure of a Letter (and also created the backend DynamoDB letter table, and installed the DynamoDB packages using pnpm

---
+++ src/types/Letter.ts
@@ -0,0 +1,10 @@
+export interface Letter {
+  id: string;
+  senderId: string;
+  recipientId: string;
+  content: string;
+  createdAt: string;
+  deliveryTime: string;
+  isRead: boolean;
+  readAt: string;
+}
