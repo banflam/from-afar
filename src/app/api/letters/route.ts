6493c03f8ed810fbfe2da5658c05ab807777c1b4
banflam <rishiagrawal2002@gmail.com>
2025-05-19 22:22:52 -0400

    added dynamodb client

---
+++ src/app/api/letters/route.ts
@@ -0,0 +1,47 @@
+import { NextRequest, NextResponse } from "next/server";
+import { v4 as uuidv4 } from "uuid";
+import { Letter } from "@/types/Letter";
+import { ddb } from "@/lib/aws/dynamo";
+import { PutCommand } from "@aws-sdk/lib-dynamodb";
+import { receiveMessageOnPort } from "worker_threads";
+import { error } from "console";
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+
+    const { senderId, recipientId, content, deliveryTime } = body;
+
+    if (!senderId || !recipientId || !content || !deliveryTime) {
+      return NextResponse.json(
+        { error: "Missing fields in the Letter" },
+        { status: 400 }
+      );
+    }
+
+    const letter: Letter = {
+      id: uuidv4(),
+      senderId,
+      recipientId,
+      content,
+      createdAt: new Date().toISOString(),
+      deliveryTime,
+      isRead: false,
+    };
+
+    await ddb.send(
+      new PutCommand({
+        TableName: "Letters",
+        Item: letter,
+      })
+    );
+
+    return NextResponse.json(letter, { status: 201 });
+  } catch (error) {
+    console.error("Error saving letter:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
