c4a4abb131f5fd61efb206be91fee5bc57e68c8e
banflam <rishiagrawal2002@gmail.com>
2025-05-20 14:49:53 -0400

    added basic state management and getting the inbox logic to the InboxPage component

--- src/app/api/inbox/page.tsx
+++ src/app/api/inbox/page.tsx
@@ -4,6 +4,24 @@ import { useEffect, useState } from "react";
 
 import { Letter } from "@/types/Letter";
 
+export default function InboxPage() {
+  const [tab, setTab] = useState<"incoming" | "unread" | "read">("incoming");
+  const [letters, setLetters] = useState<{
+    incoming: Letter[];
+    unread: Letter[];
+    read: Letter[];
+  }>({ incoming: [], unread: [], read: [] });
+
+  const recipientId = "test-recipient"; //TODO: replace with read user ID in the future
+
+  useEffect(() => {
+    fetch(`/api/inbox?recipientId=${recipientId}`)
+      .then((res) => res.json())
+      .then((data) => setLetters(data));
+  }, []);
+  
+}
+
 function formatTimeDiff(deliveryTime: Date) {
   const diff = deliveryTime.getTime() - Date.now();
   if (diff <= 0) return "Now";
