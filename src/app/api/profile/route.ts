343716dd62d8e3c7271fe4b3f0295849a1a51d21
banflam <rishiagrawal2002@gmail.com>
2025-05-24 18:14:22 -0400

    added profile route that will save the user into the dynamodb table upon user creation, because AWS Cognito is not a user database

---
+++ src/app/api/profile/route.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+import { ddb } from "@/lib/aws/dynamo";
+import { PutCommand } from "@aws-sdk/lib-dynamodb";
+import jwtDecode from "jwt-decode";
+
+export async function POST(req: NextRequest) {
+  const auth = req.headers.get("Authorization");
+  if (!auth)
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+
+  const token = auth.replace("Bearer", "");
+  const decoded: any = jwtDecode(token); // will contain the Cognito claims
+
+  const user = {
+    userId: decoded.sub,
+    username: decoded["cognito:username"],
+    city: "unknown",
+    gender: "other",
+    bio: "",
+    createdAt: new Date().toISOString(),
+  };
+
+  await ddb.send(
+    new PutCommand({
+      TableName: "Users",
+      Item: user,
+      ConditionExpression: "attribute_not_exists(userId)", // do not overwriter an existing userId
+    })
+  );
+
+  return NextResponse.json({ message: "Profile initialized" });
+}
