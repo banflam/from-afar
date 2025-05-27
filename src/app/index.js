c47b4bcccb417d1f5d696ceb17e734b78f779e60
banflam <rishiagrawal2002@gmail.com>
2025-05-26 20:56:33 -0400

    updated index.js with OIDC properties of the new user pool

--- src/app/index.js
+++ src/app/index.js
@@ -6,11 +6,11 @@ import { AuthProvider } from "react-oidc-context";
 import "./globals.css";
 
 const cognitoAuthConfig = {
-  authority: "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_XymIyz8DE",
-  client_id: "367g3sp8609p8pf8e22p13n3ta",
+  authority: "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_kevkQoqOa",
+  client_id: "6o2uld1loe0t9f07smc7sb98n1",
   redirect_uri: "https://d84l1y8p4kdic.cloudfront.net",
   response_type: "code",
-  scope: "email openid phone",
+  scope: "phone openid email",
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
