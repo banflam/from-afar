76bbcb7eaf76b63441af1b0a2d2b92d27ec3e921
banflam <rishiagrawal2002@gmail.com>
2025-05-26 21:17:03 -0400

    found that the AuthProvidedWrapper had its own definition of cognitoAuthConfig so updated that

--- src/components/AuthProvidedWrapper.tsx
+++ src/components/AuthProvidedWrapper.tsx
@@ -4,11 +4,11 @@ import { AuthProvider } from "react-oidc-context";
 import React from "react";
 
 const cognitoAuthConfig = {
-  authority: "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_XymIyz8DE",
-  client_id: "367g3sp8609p8pf8e22p13n3ta",
-  redirect_uri: "http://localhost:3000", // or your production domain
+  authority: "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_kevkQoqOa",
+  client_id: "6o2uld1loe0t9f07smc7sb98n1",
+  redirect_uri: "https://d84l1y8p4kdic.cloudfront.net",
   response_type: "code",
-  scope: "openid email phone",
+  scope: "phone openid email",
 };
 
 export default function AuthProviderWrapper({
