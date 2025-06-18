45ec33fa704c22ac30531c83f54a09f661a4c6a3
banflam <rishiagrawal2002@gmail.com>
2025-06-18 11:48:35 -0400

    removed references to localhost and changed it to the actual website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,8 @@ export default function HomePage() {
 
   const signOutRedirect = () => {
     const clientId = "6o2uld1loe0t9f07smc7sb98n1";
-    const logoutUri = "http://localhost:3000";
+    //const logoutUri = "http://localhost:3000";
+    const logoutUri = "https://from-afar.vercel.app/";
     const cognitoDomain =
       "https://us-east-2kevkqoqoa.auth.us-east-2.amazoncognito.com";
     window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(
--- src/lib/auth/config.ts
+++ src/lib/auth/config.ts
@@ -1,7 +1,8 @@
 export const cognitoAuthConfig = {
   authority: "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_kevkQoqOa",
   client_id: "6o2uld1loe0t9f07smc7sb98n1",
-  redirect_uri: "http://localhost:3000",
+  //redirect_uri: "http://localhost:3000",
+  redirect_uri: "https://from-afar.vercel.app/",
   response_type: "code",
   scope: "email openid phone",
 };
