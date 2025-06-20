cbe3b46cb4879fae05eeeb23ec50f64378031d28
banflam <rishiagrawal2002@gmail.com>
2025-06-19 23:59:32 -0400

    further obsolete comments

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -2,8 +2,6 @@ import { NextResponse } from "next/server";
 import { ddb } from "@/lib/aws/dynamo";
 import { ScanCommand } from "@aws-sdk/lib-dynamodb";
 
-// TODO: find a way to cache the data / scan less frequently since DynamoDB table scans are the most expensive and time consuming!
-// TODO: there must be a better way to get a list of all the users. This is kind of ridiculous and bug-prone, to create an entirely separate DynamoDB table for users, add info to it at account creation, retrieve info etc.
 export async function GET() {
   const res = await ddb.send(
     new ScanCommand({
